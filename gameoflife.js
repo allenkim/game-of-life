f92797353a983e340af2532deeafdfdfc2559047
Allen Kim <allenkim95@gmail.com>
2016-11-04 15:06:20 -0400

    Implement basic game of life

--- gameoflife.js
+++ gameoflife.js
@@ -31,5 +31,52 @@ function drawGrid(){
   }
 }
 
+function numNeighbors(row,col){
+  var count = 0;
+  var dr = [-1,-1,-1,0,0,1,1,1];
+  var dc = [-1,0,1,-1,1,-1,0,-1];
+  for (var i = 0; i < 8; i++){
+    if (grid[row+dr[i]])
+      if (grid[row+dr[i]][col+dc[i]])
+        count++;
+  }
+  return count;
+}
+
+function evolveGrid(){
+  var next = [];
+  for (var row = 0; row < height; row++){
+    next.push(grid[row].slice());
+  }
+  var changed = false;
+  for (var row = 0; row < height; row++){
+    for (var col = 0; col < width; col++){
+      var alive = (grid[row][col] == 1);
+      var cellNeighbors = numNeighbors(row,col);
+      if (alive && (cellNeighbors < 2 || cellNeighbors > 3)){
+        next[row][col] = 0;
+        changed = true;
+      }
+      else if (!alive && cellNeighbors == 3){
+        next[row][col] = 1;
+        changed = true;
+      }
+    }
+  }
+  grid = next;
+  return changed;
+}
+
+function startSimulation(){
+  var day = 1;
+  var tick = setInterval(function(){
+    if (evolveGrid())
+      drawGrid();
+    else
+      clearInterval(tick);
+    console.log(day++);
+  },100);
+}
+
 initializeRandomGrid();
-drawGrid();
+startSimulation();
